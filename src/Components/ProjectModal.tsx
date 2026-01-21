14a495899047131490d0c58326ace832a0cf788f
DilshanWA <dilshanpersonaluse@gmail.com>
2026-01-21 11:28:19 +0530

    update ui error

--- src/Components/ProjectModal.tsx
+++ src/Components/ProjectModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { PiGithubLogoBold } from "react-icons/pi";
 import { LuRadio } from "react-icons/lu";
 
