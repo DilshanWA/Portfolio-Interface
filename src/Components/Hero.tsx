8dc22a9efeb1794ddd5c0c130248707c6b52a52f
Dilshan Thathsara <146836586+DilshanWA@users.noreply.github.com>
2026-02-20 09:07:08 +0530

    Update Hero.tsx

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import heroImage from '../assets/myimage2.png'
+import heroImage from '../assets/myimage3.png'
 import { FaGithub,FaLinkedin, FaFacebookSquare   } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { BsMedium } from "react-icons/bs";
